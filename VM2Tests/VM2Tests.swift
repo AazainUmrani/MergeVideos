1647a77ed337bc4688507606fc65a3391f64216d
Aazain Umrani <aazainumrani@gmail.com>
2024-12-20 02:06:09 +0500

    Initial Commit

---
+++ VM2Tests/VM2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  VM2Tests.swift
+//  VM2Tests
+//
+//  Created by Apple on 20/12/2024.
+//
+
+import Testing
+@testable import VM2
+
+struct VM2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
